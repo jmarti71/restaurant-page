40a2a54f75a19e5556ca63c7b528f6619e3bd1fd
Jared <Jared.martin93@Yahoo.com>
2022-08-24 12:35:08 -0500

    reduce global code using factory IIFE

--- src/index.js
+++ src/index.js
@@ -4,39 +4,42 @@ import { loadHome } from './home';
 import { loadMenu } from './menu';
 import { loadContact } from './contact';
 
-const content = document.getElementById("content");
-loadPage(content);
-const bodyContent = renderBodyElem(content);
-loadHome(bodyContent);
-renderFooterElem(content);
+const contentRef = (() => {
+    const content = document.getElementById("content");
+    loadPage(content);
+    const bodyContent = renderBodyElem(content);
+    loadHome(bodyContent);
+    renderFooterElem(content);
+    return { bodyContent };
+})();
 
 const homeTab = (homeButton, menuButton, contactButton) => {
-    if(homeButton.id !== 'selected'){
+    if (homeButton.id !== 'selected') {
         homeButton.id = 'selected';
         menuButton.id = '';
         contactButton.id = '';
-        removeContent(bodyContent);
-        loadHome(bodyContent);
+        removeContent(contentRef.bodyContent);
+        loadHome(contentRef.bodyContent);
     }
 }
 
 const menuTab = (menuButton, homeButton, contactButton) => {
-    if(menuButton.id !== 'selected'){
+    if (menuButton.id !== 'selected') {
         menuButton.id = 'selected';
         homeButton.id = '';
         contactButton.id = '';
-        removeContent(bodyContent);
-        loadMenu(bodyContent);
+        removeContent(contentRef.bodyContent);
+        loadMenu(contentRef.bodyContent);
     }
 }
 
 const contactTab = (contactButton, homeButton, menuButton) => {
-    if(contactButton.id !== 'selected'){
+    if (contactButton.id !== 'selected') {
         contactButton.id = 'selected';
         menuButton.id = '';
         homeButton.id = '';
-        removeContent(bodyContent);
-        loadContact(bodyContent);
+        removeContent(contentRef.bodyContent);
+        loadContact(contentRef.bodyContent);
     }
 }
 
